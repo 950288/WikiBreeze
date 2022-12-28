b892084caf9526d53c18bccd011811cb45a7cefc
950288 <950288s@gamil.com>
2022-12-28 11:25:48 +0800

    config

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Menu from "@/components/Menu.vue";
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import { routes } from "@/main";
+
+</script>
+
+<template>
+  <Menu  :routes="routes" />
+  <router-view />
+</template>
+
+<style lang="scss">
+
+@import "@/style/index.scss";
+// @include set-color-mode(light);
+* {
+  margin: 0;
+  padding: 0;
+  // background-color: var(--has-background-success);
+}
+:root {
+  background-color: var(--has-background-white);
+}
+</style>
