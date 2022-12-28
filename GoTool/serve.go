613bd1abc3ef6e08b8540f793f4dde0f6491c310
950288 <950288s@gamil.com>
2022-12-28 22:46:26 +0800

    excellent golang server

---
+++ GoTool/serve.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"os"
+	"regexp"
+	"io/ioutil"
+	"strings"
+	"encoding/json"
+)
+// TreeNode represents a node in the tree
+type TreeNode struct {
+	Subtree []*pages
+}
+type pages struct {
+	name string
+	node []*TreeNode
+}
+func main() {
+	dir := "D:\\github\\web\\src\\pages"
+	re := regexp.MustCompile(`<!--\s*iGEMGotool\s*(?P<name>\S+)\s*start-->`)
+
+	// Create map to store data
+	data := make(map[string][]string)
+
+	//scan *.vue in dir
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".vue" {
+			// Read file contents
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			// Extract "name" value from file contents
+			matches := re.FindAllStringSubmatch(string(b) ,-1)
+
+			// Store file name and "name" values in map
+			fileName := strings.TrimSuffix(filepath.Base(path), ".vue")
+			nameValues := make([]string, len(matches))
+
+
+			for i , match := range matches {
+				nameValues[i] = match[1]
+			}
+			data[fileName] = nameValues
+		}
+		return nil
+	})
+
+	// Serialize map to JSON string
+	jsonData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	fmt.Println(string(jsonData) +"\n")
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		fmt.Fprintf(w, string(jsonData))
+		fmt.Println("some one visit")
+	})
+	http.HandleFunc("/getnode", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		
+		type Data struct {
+			page  string 	`json:"page"`
+			node string		`json:"node"`
+		}
+		var data Data
+
+		b := make([]byte, r.ContentLength)
+		r.Body.Read(b)
+		fmt.Println(b)
+		fmt.Println(string(b))
+		err := json.Unmarshal([]byte(string(b)), &data)
+		fmt.Println(err)
+		fmt.Println(data)
+		page := r.URL.Query().Get("page")
+		node := r.URL.Query().Get("node")
+
+		// fmt.Fprintf(w, string(jsonData))
+		fmt.Println("edit ",node," on",page)
+
+		fmt.Fprintf(w, "fuck you")
+
+	})
+	http.Handle("/", http.FileServer(http.Dir("../dist")))
+	http.ListenAndServe(":8082", nil)
+
+}
+
+// `<!-- iGEM-ToolBox:WIKI {{name}} start-->`
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,29 +1,61 @@
 <template>
     <div class="content is-large">
         <br>
-        <div class="box">
+
+        <div class="box" v-for="(page, index) in data">
+            <h2>{{ index }}</h2>
+            <div class="buttons">
+                <button v-for="node in page" class="button is-primary is-outlined"
+                    @click="editnode(index, node)">{{ node }}</button>
+                <!-- <button class="button is-primary is-outlined">About_2</button> -->
+            </div>
+        </div>
+        <!-- <div class="box">
             <h2>About</h2>
             <div class="buttons">
                 <button class="button is-primary is-outlined">About_1</button>
                 <button class="button is-primary is-outlined">About_2</button>
             </div>
-        </div>
-        <div class="box">
-            <h2>About</h2>
-            <div class="buttons">
-                <button class="button is-primary is-outlined">About_1</button>
-                <button class="button is-primary is-outlined">About_2</button>
-            </div>
-        </div>
-        <article class="message is-warning is-large">
+        </div> -->
+        <article v-if="error" class="message is-warning is-large">
             <div class="message-body">
                 <h3>fetch page list error!</h3>
+                <p>{{ error }}</p>
                 <p>please refresh this page</p>
             </div>
         </article>
-        <progress class="progress is-large is-info" max="100">60%</progress>
+        <progress v-if="!data" class="progress is-large is-info" max="100">60%</progress>
+        <!-- <div>
+            {{ data }}<br>
+        </div> -->
     </div>
 </template>
+<script setup lang="ts">
+import { useFetch } from "@vueuse/core";
+import { ref, watch } from "vue";
+const getdata = ref(false);
+const geterror = ref(false);
+let { data, error } = useFetch("http://127.0.0.1:8082/list").get().json();
+function editnode(index, node) {
+    let { data, error } = useFetch("http://127.0.0.1:8082/getnode", {
+        method: 'POST',
+        body: JSON.stringify({
+            page: index,
+            node: node
+        }),
+        headers: {
+        'Content-Type': 'application/json'
+    }
+    }).get().json();
+    watch(data, (val) => {
+        console.log(val);
+    });
+    watch(error, (val) => {
+        console.log(val);
+    });
+    console.log(data);
+}
+</script>
 <style lang="scss" scoped>
 .box {
     height: 40px;
@@ -63,7 +95,8 @@
 
 .message {
     background: var(--is-warning-gb);
-    .message-body *{
+
+    .message-body * {
         // color: #000;
     }
 }
