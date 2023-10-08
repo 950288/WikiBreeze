fa439362c1395a82d581bf3da02dff1fcf8d6fe2
950288 <950288s@gmail.com>
2023-10-08 15:09:50 +0800

    v1.1.0 Fix known bugs and improve display

--- BackEnd/serve.go
+++ BackEnd/serve.go
@@ -39,7 +39,7 @@ func main() {
 		http.Handle("/", http.FileServer(http.Dir("../Wikibreeze")))
 	}
 	fmt.Println("")
-	utils.PrintSuccess("WikiBreeze(v1.1.0-beta.5) started on port " + strconv.Itoa(port) + " successfully")
+	utils.PrintSuccess("WikiBreeze(v1.1.0) started on port " + strconv.Itoa(port) + " successfully")
 	fmt.Println("Local:\t\t", utils.Cyanf("http://localhost:"+strconv.Itoa(port)+"/"))
 
 	//Get local ip
